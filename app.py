2eacc41d7db31d7852d580cc795443879b83fecc
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-17 15:16:17 -0800

    feat: add flash message
    (Completed step 7.)
    
    * Added a Flask flash message when a user accesses a question page the
    user is not supposed to be on.  The category will be used as an HTML
    class for CSS styling.
    
    * Added the basic HTML in "question.html" to display the flash message.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import *
@@ -19,6 +19,7 @@ def route_root():
 @app.route("/questions/<int:ques_num>")
 def route_questions(ques_num):
     if ques_num != len(responses):
+        flash("Invalid question.  Redirecting to the correct URL.", "status-message--error")
         return redirect(__next_page__())
     else:
         return render_template(
