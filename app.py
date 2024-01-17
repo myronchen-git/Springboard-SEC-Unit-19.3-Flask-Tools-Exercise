2c6a2691121896cd5fc10c73f16adeae64e3a331
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-17 15:41:40 -0800

    chore: add function descriptions and rename root
    * Added missing function descriptions.
    
    * Renamed "root.html" to "index.html" for possible GitHub page
    deploying.

--- app.py
+++ app.py
@@ -14,10 +14,14 @@ responses = []
 
 @app.route("/")
 def route_root():
-    return render_template("root.html", survey=satisfaction_survey)
+    """Display the start of the survey."""
+
+    return render_template("index.html", survey=satisfaction_survey)
 
 @app.route("/questions/<int:ques_num>")
 def route_questions(ques_num):
+    """Display a question."""
+
     if ques_num != len(responses):
         flash("Invalid question.  Redirecting to the correct URL.", "status-message--error")
         return redirect(__next_page__())
@@ -27,16 +31,22 @@ def route_questions(ques_num):
 
 @app.route("/answer", methods=["post"])
 def route_answer():
+    """Handle an answer."""
+
     responses.append(request.form["answer"])
     return redirect(__next_page__())
 
 @app.route("/thankyou")
 def route_thankyou():
+    """Display a survey completion page."""
+
     return render_template("thankyou.html")
 
 # ==================================================
 
 def __next_page__():
+    """Helper function to find the next page to go to."""
+
     if len(responses) < len(satisfaction_survey.questions):
         return f"/questions/{len(responses)}"
     else:
