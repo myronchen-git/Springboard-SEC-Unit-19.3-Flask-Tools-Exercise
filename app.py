d0e500010fb5e03508a7a2c1d5b1672eea700772
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-18 14:34:23 -0800

    style: format Python files
    No functional changes were made.

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret"
+app.config["SECRET_KEY"] = "secret"
 
 debug = DebugToolbarExtension(app)
 
 # ==================================================
+
 
 @app.route("/")
 def route_root():
     """Display the start of the survey."""
 
     return render_template("index.html", survey=satisfaction_survey)
+
 
 @app.route("/questions", methods=["post"])
 def route_questions():
@@ -23,19 +25,25 @@ def route_questions():
     session["responses"] = []
     return redirect("/questions/0")
 
+
 @app.route("/questions/<int:ques_num>")
 def route_question_num(ques_num):
     """Display a question."""
 
     if ques_num != len(session["responses"]):
-        flash("Invalid question.  Redirecting to the correct URL.", "status-message--error")
+        flash(
+            "Invalid question.  Redirecting to the correct URL.",
+            "status-message--error",
+        )
         return redirect(__next_page__())
     else:
         return render_template(
             "question.html",
             survey_title=satisfaction_survey.title,
             question_number=ques_num,
-            question=satisfaction_survey.questions[ques_num])
+            question=satisfaction_survey.questions[ques_num],
+        )
+
 
 @app.route("/answer", methods=["post"])
 def route_answer():
@@ -47,13 +55,16 @@ def route_answer():
 
     return redirect(__next_page__())
 
+
 @app.route("/thankyou")
 def route_thankyou():
     """Display a survey completion page."""
 
     return render_template("thankyou.html", survey_title=satisfaction_survey.title)
 
+
 # ==================================================
+
 
 def __next_page__():
     """Helper function to find the next page to go to."""
