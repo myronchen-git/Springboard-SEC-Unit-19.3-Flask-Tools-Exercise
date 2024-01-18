9556ac5189d92a741d3093ac66a009a3e6d081e6
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-18 13:23:03 -0800

    feat: utilize session cookie
    (Completed step 8.)
    
    * Changed the responses List from being stored within the app's Python
    code to being stored in a user's cookie, by using Flask session.
    
    * Changed the start page to use a form and a post method to begin the
    survey, per instructions.
    
    * Added new route, "/questions", to initialize the responses List in a
    Flask session cookie.  As a result, also changed the function name for
    displaying a question from "route_questions" to "route_question_num".

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import *
@@ -10,19 +10,24 @@ debug = DebugToolbarExtension(app)
 
 # ==================================================
 
-responses = []
-
 @app.route("/")
 def route_root():
     """Display the start of the survey."""
 
     return render_template("index.html", survey=satisfaction_survey)
 
+@app.route("/questions", methods=["post"])
+def route_questions():
+    """Set up environment and redirect to first question."""
+
+    session["responses"] = []
+    return redirect("/questions/0")
+
 @app.route("/questions/<int:ques_num>")
-def route_questions(ques_num):
+def route_question_num(ques_num):
     """Display a question."""
 
-    if ques_num != len(responses):
+    if ques_num != len(session["responses"]):
         flash("Invalid question.  Redirecting to the correct URL.", "status-message--error")
         return redirect(__next_page__())
     else:
@@ -33,7 +38,10 @@ def route_questions(ques_num):
 def route_answer():
     """Handle an answer."""
 
+    responses = session["responses"]
     responses.append(request.form["answer"])
+    session["responses"] = responses
+
     return redirect(__next_page__())
 
 @app.route("/thankyou")
@@ -47,7 +55,9 @@ def route_thankyou():
 def __next_page__():
     """Helper function to find the next page to go to."""
 
-    if len(responses) < len(satisfaction_survey.questions):
-        return f"/questions/{len(responses)}"
+    responses_length = len(session["responses"])
+
+    if responses_length < len(satisfaction_survey.questions):
+        return f"/questions/{responses_length}"
     else:
         return "/thankyou"
